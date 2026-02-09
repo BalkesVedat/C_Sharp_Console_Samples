aae90303bb62e46f625e81eb2a61bd44c3dfbb37
BalkesVedat <80820828+BalkesVedat@users.noreply.github.com>
2026-02-09 19:02:37 +0300

    v4

---
+++ KonsolUygulamalarım/OOP/Classes/Canli.cs
@@ -0,0 +1,30 @@
+﻿using OOP.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Classes
+{
+    internal class Canli : ICanli
+    {
+        public string Name { get ; set ; }
+        public Cinsiyet Tur { get ; set; }
+
+        public string Beslen()
+        {
+            return "Canlı Beslendi";
+        }
+
+        public string Dead()
+        {
+            return "Canlı Öldü";
+        }
+
+        public string Ure()
+        {
+            return "Canlı Üredi.";
+        }
+    }
+}
---
+++ KonsolUygulamalarım/OOP/Interfaces/ICanli.cs
@@ -0,0 +1,20 @@
+﻿using OOP.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal interface ICanli
+    {
+        string Name { get; set; }
+        Cinsiyet Tur { get; set; }
+
+        string Beslen();
+        string Ure();
+        string Dead();
+
+    }
+}
